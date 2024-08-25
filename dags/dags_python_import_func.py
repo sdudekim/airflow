74e9f55a8af7f958347de5cb0902595429afad0b
sdudekim <walnam777@naver.com>
2024-08-25 12:07:18 +0900

    python import test modify

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 from common.common_func import get_sftp
 
 with DAG(
-    dag_id=dags_python_import_func,
+    dag_id="dags_python_import_func",
     schedule="0 1 1 * *",
     start_date=pendulum.datetime(2024,1,1,tz="Asia/Seoul"),
     catchup=False
