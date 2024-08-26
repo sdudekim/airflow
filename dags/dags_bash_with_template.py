dc4b10f243be186138aebbc1082b05ba6b066c58
sdudekim <walnam777@naver.com>
2024-08-26 20:44:34 +0900

    bash template test

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -14,14 +14,14 @@ with DAG(
 
     bash_t1 = BashOperator(
         task_id="bash_t1",
-        bash_command=' echo "date_interval_end: {{date_interval_end}} " '
+        bash_command='echo "data_interval_end: {{data_interval_end}} "'
     )
 
     bash_t2 = BashOperator(
         task_id='bash_t2',
         env={
-            'START_DATE' : '{{date_interval_start | ds}}',
-            'END_DATE': '{{date_interval_end | ds}}'
+            'START_DATE' : '{{data_interval_start | ds}}',
+            'END_DATE': '{{data_interval_end | ds}}'
         },
         bash_command='echo $START_DATE && echo $END_DATE'
     )
